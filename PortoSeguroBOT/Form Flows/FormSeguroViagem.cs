5204a333cab2223050e9bad33178a64a85bfec63
F0113091 <F0113091@01-14371N733301>
2016-11-01 18:33:51 -0200

    estado e pais no form de seguro

--- PortoSeguroBOT/Form Flows/FormSeguroViagem.cs
+++ PortoSeguroBOT/Form Flows/FormSeguroViagem.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.FormFlow;
+using PortoSeguroBOT.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using static PortoSeguroBOT.Helpers.ConstantsPais;
+using static PortoSeguroBOT.Helpers.ConstantsUF;
 
 namespace PortoSeguroBOT.Form_Flows
 {
@@ -13,8 +16,12 @@ namespace PortoSeguroBOT.Form_Flows
     public class FormSeguroViagem
     {
         [Prompt("Qual o estado de origem da sua viagem? {||}")]
-        [Template(TemplateUsage.NotUnderstood, "Estado Inválido")]
+        [Template(TemplateUsage.NotUnderstood, "Estado Inválidox")]
         public string Origem;
+
+        [Prompt("Qual o país de destino da sua viagem? {||}")]
+        [Template(TemplateUsage.NotUnderstood, "País Inválidox")]
+        public string Destino;
 
         public static IForm<FormSeguroViagem> SeguroBuildForm()
         {
@@ -22,11 +29,12 @@ namespace PortoSeguroBOT.Form_Flows
             OnCompletionAsyncDelegate<FormSeguroViagem> processandoCalculo = async (context, state) =>
             {
                 //await context.PostAsync($"Ok. Calculando seguro para {state.Destino} de {state.Origem} indo no dia {state.DataPartida} e voltando no dia {state.DataRetorno}");
-                await context.PostAsync($"Fim de formulário, recebi {state.Origem}");
+                await context.PostAsync($"Fim de formulário, recebi {state.Origem} e {state.Destino}");
             };
 
             return new FormBuilder<FormSeguroViagem>()
                 .Field(nameof(Origem), validate: ValidateUF)
+                .Field(nameof(Destino), validate: ValidatePais)
                 .OnCompletion(processandoCalculo)
                 .Build();
         }
@@ -39,19 +47,70 @@ namespace PortoSeguroBOT.Form_Flows
                 Value = value
             };
 
-            if (!"SP".Equals(value))
+            Estado estado = GetEstado(value.ToString());
+            if (estado == null)
             {
                 result.Feedback = "Estado Inválido";
                 result.IsValid = false;
             }
             else
             {
-                result.Value = "NOVO VALOR";
+                result.Value = estado.Descricao;
+                ctx
             }
 
             return Task.FromResult(result);
         }
 
-        public enum EstadoOrigem {SP, RJ, ES, MG, PE, SC, RS}
+        private static Task<ValidateResult> ValidatePais(FormSeguroViagem state, object value)
+        {
+            var result = new ValidateResult
+            {
+                IsValid = true,
+                Value = value
+            };
+
+            Pais pais = GetPais(value.ToString());
+            if (pais == null)
+            {
+                result.Feedback = "Pais Inválido";
+                result.IsValid = false;
+            }
+            else
+            {
+                result.Value = pais.Descricao;
+            }
+
+            return Task.FromResult(result);
+        }
+
+
+        public static Estado GetEstado(string valor)
+        {
+            Estado retorno = null;
+            foreach (Estado estado in ConstantsUF.ESTADOS)
+            {
+                if (estado.Sigla.Equals(valor, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    retorno = estado;
+                    break;
+                }
+            }
+            return retorno;
+        }
+
+        public static Pais GetPais(string valor)
+        {
+            Pais retorno = null;
+            foreach (Pais pais in ConstantsPais.PAISES)
+            {
+                if (pais.Descricao.Equals(valor, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    retorno = pais;
+                    break;
+                }
+            }
+            return retorno;
+        }
     }
 }
---
+++ PortoSeguroBOT/Helpers/ConstantsUF.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortoSeguroBOT.Helpers
+{
+    public class ConstantsUF
+    {
+        public static readonly Estado[] ESTADOS = new Estado[]
+        {
+            new Estado("AC", "Acre"),
+            new Estado("AL", "Alagoas"),
+            new Estado("AM", "Amazonas"),
+            new Estado("AP", "Amapá"),
+            new Estado("BA", "Bahia"),
+            new Estado("CE", "Ceará"),
+            new Estado("DF", "Distrito Federal"),
+            new Estado("ES", "Espírito Santo"),
+            new Estado("GO", "Goiás"),
+            new Estado("MA", "Maranhão"),
+            new Estado("MG", "Minas Gerais"),
+            new Estado("MS", "Mato Grosso do Sul"),
+            new Estado("MT", "Mato Grosso"),
+            new Estado("PA", "Pará"),
+            new Estado("PB", "Paraíba"),
+            new Estado("PE", "Pernambuco"),
+            new Estado("PI", "Piauí"),
+            new Estado("PR", "Paraná"),
+            new Estado("RJ", "Rio de Janeiro"),
+            new Estado("RN", "Rio Grande do Norte"),
+            new Estado("RO", "Rondônia"),
+            new Estado("RR", "Roraima"),
+            new Estado("RS", "Rio Grande do Sul"),
+            new Estado("SC", "Santa Catarina"),
+            new Estado("SE", "Sergipe"),
+            new Estado("SP", "São Paulo"),
+            new Estado("TO", "Tocantins")
+        };
+
+        public class Estado
+        {
+            //TODO arrumar get set
+            public string Sigla { get; set; }
+            public string Descricao { get; set; }
+
+            public Estado(string Sigla, string Descricao)
+            {
+                this.Sigla = Sigla;
+                this.Descricao = Descricao;
+            }
+
+            public override string ToString()
+            {
+                return String.Concat(Sigla, Descricao);
+            }
+        }
+    }
+}
