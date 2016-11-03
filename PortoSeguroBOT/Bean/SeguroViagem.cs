ca3f102bedbf868a648b08088592ca648b147d29
Caio <barroso.pc@gmail.com>
2016-11-03 17:03:22 -0200

    Perguntas Formulário, dados no context

---
+++ PortoSeguroBOT/Bean/SeguroViagem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortoSeguroBOT.Bean
+{
+    public class SeguroViagem
+    {
+        public string UfOrigem { get; set; }
+        public string EstadoOrigem { get; set; }
+        public string PaisDestino { get; set; }
+        public int CodPaisDestino { get; set; }
+        public Boolean PaisEuropeuDestino { get; set; }
+        public DateTime DataPartida { get; set; }
+        public DateTime DataRetorno { get; set; }
+        public int Menor70 { get; set; }
+        public int Maior70 { get; set; }
+        public Boolean EsporteAventura { get; set; }
+        public string Motivo { get; set; }
+        public int CodMotivo { get; set; }
+    }
+}
--- PortoSeguroBOT/Form Flows/FormSeguroViagem.cs
+++ PortoSeguroBOT/Form Flows/FormSeguroViagem.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.FormFlow;
+using PortoSeguroBOT.Bean;
 using PortoSeguroBOT.Helpers;
 using System;
 using System.Collections.Generic;
@@ -16,7 +17,7 @@ namespace PortoSeguroBOT.Form_Flows
     public class FormSeguroViagem
     {
         [Prompt("Qual o estado de origem da sua viagem? {||}")]
-        [Template(TemplateUsage.NotUnderstood, "Estado Inválidox")]
+        [Template(TemplateUsage.NotUnderstood, "Estado Inválido")]
         public string Origem;
 
         [Prompt("Qual o país de destino da sua viagem? {||}")]
@@ -27,19 +28,50 @@ namespace PortoSeguroBOT.Form_Flows
         [Template(TemplateUsage.NotUnderstood, "Não entendi a sua resposta")]
         public string PaisEuropeu;
 
+        [Prompt("Qual a data da sua partida? Escreva no padrão dd/mm/aaaa")]
+        [Template(TemplateUsage.NotUnderstood, "Eu não entendi \"{0}\".", "Tente novamente com o formato dd/mm/aaaa, eu não entendi \"{0}\".")]
+        [Pattern(@"^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$")]
+        public DateTime DataPartida { get; set; }
+
+        [Prompt("Qual a data do seu retorno? Escreva no padrão dd/mm/aaaa")]
+        [Template(TemplateUsage.NotUnderstood, "Eu não entendi \"{0}\".", "Tente novamente com o formato dd/mm/aaaa, eu não entendi \"{0}\".")]
+        [Pattern(@"^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$")]
+        public DateTime DataRetorno { get; set; }
+
+        [Prompt("Quantos passageiros com menos de 70 anos (inclua o passageiro principal)? {||}")]
+        [Template(TemplateUsage.NotUnderstood, "Quantidade inválida")]
+        public string Menor70;
+
+        [Prompt("Quantos passageiros com mais de 70 anos (inclua o passageiro principal)? {||}")]
+        [Template(TemplateUsage.NotUnderstood, "Quantidade inválida")]
+        public string Maior70;
+
+        [Prompt("Essa viagem irá incluir a prática de algum esporte ou aventura? {||}")]
+        [Template(TemplateUsage.NotUnderstood, "Não entendi sua resposta")]
+        public string ViagemAventura;
+
+        [Prompt("Qual o motivo da sua viagem? {||}")]
+        public MotivoViagemOptions? MotivoDaViagem;
+
         public static IForm<FormSeguroViagem> SeguroBuildForm()
         {
             //Callback para final de formulário
             OnCompletionAsyncDelegate<FormSeguroViagem> processandoCalculo = async (context, state) =>
             {
-                //await context.PostAsync($"Ok. Calculando seguro para {state.Destino} de {state.Origem} indo no dia {state.DataPartida} e voltando no dia {state.DataRetorno}");
-                await context.PostAsync($"Fim de formulário, recebi {state.Origem} e {state.Destino}");
+                context.UserData.SetValue("DadosSeguroViagem", PopulateContextWithData(state));
+                await context.PostAsync($"Fim de formulário, calculando seguro");
             };
 
             return new FormBuilder<FormSeguroViagem>()
                 .Field(nameof(Origem), validate: ValidateUF)
                 .Field(nameof(Destino), validate: ValidatePais)
-                .Field(nameof(PaisEuropeu),validate: ValidatePaisEuropeu)
+                .Field(nameof(PaisEuropeu),validate: ValidateSimNao)
+                .Field(nameof(DataPartida), validate: ValidateStartDate)
+                .Field(nameof(DataRetorno), validate: ValidateEndDate)
+                .Field(nameof(Menor70), validate: ValidateQtd)
+                .Field(nameof(Maior70), validate: ValidateQtd)
+                .Field(nameof(ViagemAventura), validate: ValidateSimNao)
+                .Field(nameof(MotivoDaViagem), validate: ValidateMotivoViagem)
                 .OnCompletion(processandoCalculo)
                 .Build();
         }
@@ -88,7 +120,7 @@ namespace PortoSeguroBOT.Form_Flows
             return Task.FromResult(result);
         }
 
-        private static Task<ValidateResult> ValidatePaisEuropeu(FormSeguroViagem state, object value)
+        private static Task<ValidateResult> ValidateSimNao(FormSeguroViagem state, object value)
         {
             var result = new ValidateResult
             {
@@ -106,6 +138,90 @@ namespace PortoSeguroBOT.Form_Flows
                 result.Value = ConstantsBase.getConfirmation(value.ToString());
             }
 
+            return Task.FromResult(result);
+        }
+
+        private static Task<ValidateResult> ValidateStartDate(FormSeguroViagem state, object value)
+        {
+            var result = new ValidateResult
+            {
+                IsValid = true,
+                Value = value
+            };
+            var startDate = (DateTime)value;
+            DateTime formattedDt;
+            if (DateTime.TryParse(startDate.ToString(new CultureInfo("pt-BR")), out formattedDt))
+            {
+                startDate = formattedDt;
+            }
+            if (startDate < DateTime.Today)
+            {
+                result.Feedback = "Data inicial inválida";
+                result.IsValid = false;
+            }
+            return Task.FromResult(result);
+        }
+
+        private static Task<ValidateResult> ValidateEndDate(FormSeguroViagem state, object value)
+        {
+            var result = new ValidateResult
+            {
+                IsValid = true,
+                Value = value
+            };
+            var startDate = state.DataPartida;
+            DateTime formattedDt;
+            if (DateTime.TryParse(state.DataPartida.ToString(new CultureInfo("pt-BR")), out formattedDt))
+            {
+                startDate = formattedDt;
+            }
+            var endDate = (DateTime)value;
+            if (DateTime.TryParse(
+                endDate.ToString(new CultureInfo("pt-BR")), out formattedDt))
+            {
+                endDate = formattedDt;
+            }
+            if (endDate < startDate)
+            {
+                result.Feedback = "Data final inválida";
+                result.IsValid = false;
+            }
+            return Task.FromResult(result);
+        }
+
+        private static Task<ValidateResult> ValidateQtd(FormSeguroViagem state, object value)
+        {
+            var result = new ValidateResult
+            {
+                IsValid = true,
+                Value = value
+            };
+            int newValue;
+            if (!int.TryParse(value.ToString(), out newValue))
+            {
+                result.Feedback = "Não entendi a sua resposta";
+                result.IsValid = false;
+            }
+            else
+            {
+                if(newValue < 0)
+                {
+                    result.Feedback = "Quantidade inválida";
+                    result.IsValid = false;
+                }
+            }
+
+            return Task.FromResult(result);
+        }
+
+        private static Task<ValidateResult> ValidateMotivoViagem(FormSeguroViagem state, object value)
+        {
+            var result = new ValidateResult
+            {
+                IsValid = true,
+                Value = value
+            };
+        
             return Task.FromResult(result);
         }
 
@@ -136,5 +252,21 @@ namespace PortoSeguroBOT.Form_Flows
             }
             return retorno;
         }
+
+        private static SeguroViagem PopulateContextWithData(FormSeguroViagem state)
+        {
+            SeguroViagem seguro = new SeguroViagem();
+            seguro.UfOrigem = state.Origem;
+            seguro.PaisDestino = state.Destino;
+            seguro.PaisEuropeuDestino = state.PaisEuropeu == "Sim"?true:false;
+            seguro.DataPartida = state.DataPartida;
+            seguro.DataRetorno = state.DataRetorno;
+            seguro.Menor70 = int.Parse(state.Menor70);
+            seguro.Maior70 = int.Parse(state.Maior70);
+            seguro.Motivo =  state.MotivoDaViagem.ToString();
+            return seguro;
+        }
+
+        public enum MotivoViagemOptions { Lazer, Negocios, Estudos, VisitaAmigoOuParente }
     }
 }
